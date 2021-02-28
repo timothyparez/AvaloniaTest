77ee14b2bae7f9d3b34b55374232ba294585d482
Michel <michelvv@vvdesign.nl>
2021-02-28 10:35:56 +0100

    DynamicData implemented

--- ViewModels/CityListViewModel.cs
+++ ViewModels/CityListViewModel.cs
@@ -3,28 +3,68 @@ using AvaloniaTest.Services;
 using DynamicData;
 using DynamicData.Binding;
 using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 
 namespace AvaloniaTest.ViewModels
 {
     public class CityListViewModel : ViewModelBase
     {
-        private string _selectedCountry = string.Empty;
-        public string SelectedCountry
+        private Country _selectedCountry;
+        public Country SelectedCountry
         {
             get => _selectedCountry;
             private set => this.RaiseAndSetIfChanged(ref _selectedCountry, value);
         }
 
+        private SourceCache<Country, int> _countryCache = new SourceCache<Country, int>(x => x.CountryId);
+        public ReadOnlyObservableCollection<Country> Countries => _countries;
+        private readonly ReadOnlyObservableCollection<Country> _countries;
+
+
+        private SourceCache<City, int> _cityCache = new SourceCache<City, int>(x => x.CityId);
+        public ReadOnlyObservableCollection<City> Cities => _cities;
+        private readonly ReadOnlyObservableCollection<City> _cities;
+
+        private readonly IDisposable _countryCleanUp;
+        private readonly IDisposable _cityCleanUp;
+
         public CityListViewModel(IEnumerable<Country> countries, IEnumerable<City> cities)
         {
-            Countries = new ObservableCollection<Country>(countries);
-            Cities = new ObservableCollection<City>(cities);
+            _countryCache.AddOrUpdate(countries);
+
+            _countryCleanUp = _countryCache.Connect()
+                .Bind(out _countries)
+                .DisposeMany()
+                .Subscribe();
+
+            _cityCache.AddOrUpdate(cities);
+
+            //Search logic
+            Func<City, bool> cityFilter(Country country) => city =>
+            {
+                return country == null || city.CountryId == SelectedCountry.CountryId;
+            };
+
+            var filterPredicate = this.WhenAnyValue(x => x.SelectedCountry)
+                                      .Throttle(TimeSpan.FromMilliseconds(250), RxApp.TaskpoolScheduler)
+                                      .DistinctUntilChanged()
+                                      .Select(cityFilter);
+
+            _cityCleanUp = _cityCache.Connect()
+                .Filter(filterPredicate)
+                .Bind(out _cities)
+                .DisposeMany()
+                .Subscribe();
         }
 
-        public ObservableCollection<Country> Countries { get; }
-        public ObservableCollection<City> Cities { get; }
+        public void Dispose()
+        {
+            _countryCleanUp.Dispose();
+            _cityCleanUp.Dispose();
+        }
     }
 }
